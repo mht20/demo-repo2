544fdc56b679e1c0b607bba5d043957c4117b7a0
user <user@users-iMac.mediacom.info>
2023-04-09 16:50:10 -0500

    Created README and other fifferent type of files
    this is Awesome

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.demorepo2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.demorepo2 to javafx.fxml;
+    exports com.example.demorepo2;
+}
